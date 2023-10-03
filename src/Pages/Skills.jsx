c2451511d2da6799bc5b13b2024367cbcff6125e
Ayush Verma <ayush98277@gmail.com>
2023-10-03 16:54:45 +0530

    added some design

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -2,7 +2,16 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 
 const Skills = () => {
-  return <Box h="100vh">Skills</Box>;
+  return (
+    <Box
+      h={{ base: "650px", md: "450px", lg: "100vh" }}
+      w="100%"
+      pt={{ base: "50px", md: "60px", lg: "65px" }}
+      // border="1px solid red"
+    >
+      Skills
+    </Box>
+  );
 };
 
 export default Skills;
