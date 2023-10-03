fa389123fab655d58acef4c6c93e28b8b988ce5b
Ayush Verma <ayush98277@gmail.com>
2023-10-03 16:25:43 +0530

    About Section Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,22 @@
-import { Box, Button, Flex, Link, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Link,
+  Grid,
+  Image,
+  Input,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Skills from "../Pages/Skills";
@@ -8,108 +25,207 @@ import Contact from "../Pages/Contact";
 
 const Navbar = () => {
   const [section, setSection] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
 
   const handleSection = (sec) => {
     setSection(sec);
+    onClose();
   };
 
   return (
-    <Box position="relative" >
+    <Box position="relative">
+      <Flex
+        h={{ base: "50px", md: "60px", lg: "65px" }}
+        w="100%"
+        alignItems={"center"}
+        pl={{ base: "10px", md: "40px", lg: "80px" }}
+        pr={{ base: "10px", md: "40px", lg: "80px" }}
+        bgColor="white"
+        boxShadow="rgba(0, 0, 0, 0.16) 0px 1px 4px"
+        zIndex={10}
+        position={"fixed"}
+        top="0"
+        justifyContent={"space-between"}
+      >
+        <Link fontWeight="500" fontSize={"lg"}>
+          Ayush Verma
+        </Link>
         <Flex
-            h={{ base: "50px", md: "65px", lg: "65px" }}
-            w="100%"
-            alignItems={"center"}
-            pl={{ base: "10px", md: "80px", lg: "80px" }}
-            pr={{ base: "10px", md: "80px", lg: "80px" }}
-            bgColor="white"
-            boxShadow="rgba(0, 0, 0, 0.16) 0px 1px 4px"
-            zIndex={10}
-            position={"fixed"}
-            top="0"
-            justifyContent={"space-between"}
+          display={{ base: "none", md: "flex", lg: "flex" }}
+          justifyContent={"space-between"}
+          ml={{ base: "10%", md: "10%", lg: "16%" }}
+          cursor={"pointer"}
+          w={{ base: "40%", md: "50%", lg: "50%" }}
+          fontWeight="500"
+          // border="1px solid black"
+          alignItems={"center"}
+          fontSize={{ base: "40%", md: "14px", lg: "md" }}
         >
-            <Link fontWeight="500" fontSize={"lg"}>
-            Ayush Verma
-            </Link>
-            <Flex
-            display={{ base: "none", md: "flex", lg: "flex" }}
-            justifyContent={"space-around"}
-            ml={{ base: "10%", md: "10%", lg: "16%" }}
-            cursor={"pointer"}
-            w={{ base: "40%", md: "45%", lg: "50%" }}
-            fontWeight="500"
-            // border="1px solid black"
-            alignItems={"center"}
+          <Link
+            href="#home"
+            color={section === "sec1" ? "teal" : "black"}
+            onClick={() => handleSection("sec1")}
+          >
+            Home
+          </Link>
+          <Link
+            href="#about"
+            color={section === "sec2" ? "teal" : "black"}
+            onClick={() => handleSection("sec2")}
+          >
+            About
+          </Link>
+          <Link
+            href="#skills"
+            color={section === "sec3" ? "teal" : "black"}
+            onClick={() => handleSection("sec3")}
+          >
+            Skills
+          </Link>
+          <Link
+            href="#projects"
+            color={section === "sec4" ? "teal" : "black"}
+            onClick={() => handleSection("sec4")}
+          >
+            Projects
+          </Link>
+          <Link
+            href="#contact"
+            color={section === "sec5" ? "teal" : "black"}
+            onClick={() => handleSection("sec5")}
+          >
+            Contact
+          </Link>
+          <Link to="https://drive.google.com/u/0/uc?id=1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as&export=download">
+            <Button
+              p="0px"
+              m="0px"
+              fontSize={{ base: "40%", md: "14px", lg: "md" }}
+              variant={"none"}
+              onClick={() =>
+                window.open(
+                  "https://drive.google.com/file/d/1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as/view?usp=sharing",
+                  "_blank",
+                  "noreferrer"
+                )
+              }
             >
-            <Link
-                href="#home"
-                color={section === "sec1" ? "teal" : "black"}
-                onClick={() => handleSection("sec1")}
-            >
-                Home
-            </Link>
-            <Link
-                href="#about"
-                color={section === "sec2" ? "teal" : "black"}
-                onClick={() => handleSection("sec2")}
-            >
-                About
-            </Link>
-            <Link
-                href="#skills"
-                color={section === "sec3" ? "teal" : "black"}
-                onClick={() => handleSection("sec3")}
-            >
-                Skills
-            </Link>
-            <Link
-                href="#projects"
-                color={section === "sec4" ? "teal" : "black"}
-                onClick={() => handleSection("sec4")}
-            >
-                Projects
-            </Link>
-            <Link
-                href="#contact"
-                color={section === "sec5" ? "teal" : "black"}
-                onClick={() => handleSection("sec5")}
-            >
-                Contact
-            </Link>
-            <Link to="https://drive.google.com/u/0/uc?id=1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as&export=download">
-                <Button
-                variant={"none"}
-                onClick={() =>
-                    window.open(
-                    "https://drive.google.com/file/d/1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as/view?usp=sharing",
-                    "_blank",
-                    "noreferrer"
-                    )
-                }
-                >
-                Resume
-                </Button>
-            </Link>
-            </Flex>
+              Resume
+            </Button>
+          </Link>
         </Flex>
 
-        <Box >
-            <section id="home">
-            <Home />
-            </section>
-            <section id="about">
-            <About />
-            </section>
-            <section id="skills">
-            <Skills />
-            </section>
-            <section id="projects">
-            <Projects />
-            </section>
-            <section id="contact">
-            <Contact />
-            </section>
+        <Box
+          ref={btnRef}
+          onClick={onOpen}
+          display={{ base: "box", md: "none", lg: "none" }}
+          ml="5px"
+        >
+          <Image src="icons8-menu-50.png" w="32px" />
         </Box>
+
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+
+            <DrawerBody>
+              <Flex
+                display={{ base: "flex", md: "none", lg: "none" }}
+                flexDirection="column"
+                justifyContent={"space-between"}
+                ml={{ base: "10%", md: "10%", lg: "16%" }}
+                cursor={"pointer"}
+                fontWeight="500"
+                // border="1px solid black"
+                alignItems={"left"}
+                fontSize={{ base: "18px", md: "14px", lg: "md" }}
+                gap="50px"
+              >
+                <Link
+                  href="#home"
+                  color={section === "sec1" ? "teal" : "black"}
+                  onClick={() => handleSection("sec1")}
+                >
+                  Home
+                </Link>
+                <Link
+                  href="#about"
+                  color={section === "sec2" ? "teal" : "black"}
+                  onClick={() => handleSection("sec2")}
+                >
+                  About
+                </Link>
+                <Link
+                  href="#skills"
+                  color={section === "sec3" ? "teal" : "black"}
+                  onClick={() => handleSection("sec3")}
+                >
+                  Skills
+                </Link>
+                <Link
+                  href="#projects"
+                  color={section === "sec4" ? "teal" : "black"}
+                  onClick={() => handleSection("sec4")}
+                >
+                  Projects
+                </Link>
+                <Link
+                  href="#contact"
+                  color={section === "sec5" ? "teal" : "black"}
+                  onClick={() => handleSection("sec5")}
+                >
+                  Contact
+                </Link>
+              </Flex>
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Link to="https://drive.google.com/u/0/uc?id=1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as&export=download">
+                <Button
+                  w="100%"
+                  size="sm"
+                  colorScheme="cyan"
+                  shadow={"md"}
+                  onClick={() =>
+                    window.open(
+                      "https://drive.google.com/file/d/1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as/view?usp=sharing",
+                      "_blank",
+                      "noreferrer"
+                    )
+                  }
+                >
+                  RESUME →
+                </Button>
+              </Link>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </Flex>
+
+      <Box>
+        <section id="home">
+          <Home />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+      </Box>
     </Box>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,62 +4,89 @@ import React from "react";
 const Home = () => {
   return (
     <Box
-      h="100vh"
+      h={{ base: "650px", md: "450px", lg: "100vh" }}
       w="100%"
-      pt={{ base: "50px", md: "65px", lg: "65px" }}
+      pt={{ base: "50px", md: "60px", lg: "65px" }}
       // border="1px solid red"
     >
-      <Flex w="100%" h="100%">
-        <Box w="50%" h="100%" borderBottom="1px dashed #0BC5EA">
-          <Box mt="150px" fontWeight="500" textAlign={"left"} ml="25%">
-            <Text fontSize={{ base: "", md: "", lg: "50px" }}>
+      <Flex w="100%" h="100%" flexDirection={{ base: "column", md: "row", lg: "row" }} >
+        <Box
+          w={{ base: "100%", md: "50%", lg: "50%" }}
+          h={{ base: "40%", md: "100%", lg: "100%" }}
+          borderBottom={{base:"none", md:"1px dashed #0BC5EA", lg:"1px dashed #0BC5EA"}}
+          position="relative"
+        >
+          <Box
+            display="inline-block"
+            whiteSpace={"nowrap"}
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            fontWeight="500"
+            textAlign={"left"}
+            // border="1px solid black"
+          >
+            <Text fontSize={{ base: "32px", md: "32px", lg: "50px" }}>
               I'm Ayush Verma
             </Text>
-            <Text fontSize={{ base: "", md: "", lg: "20px" }} color={"#00ACD5"}>
+            <Text fontSize={{ base: "14px", md: "14px", lg: "20px" }} color={"#00ACD5"}>
               Aspiring Full Stack Web Developer
             </Text>
-            <Text fontSize={{ base: "", md: "", lg: "20px" }} color={"#00ACD5"}>
+            <Text fontSize={{ base: "14px", md: "14px", lg: "20px" }} color={"#00ACD5"}>
               at Masai School, Banglore
             </Text>
+
+            <Flex m="auto" mt="20px">
+              <Button
+                // w="fit-content"
+                size="sm"
+                borderRightRadius={"none"}
+                colorScheme="cyan"
+                shadow={"md"}
+              >
+                MORE ABOUT ME →
+              </Button>
+              <Button
+                // w="fit-content"
+                size="sm"
+                borderLeftRadius={"none"}
+                variant={"outline"}
+                colorScheme="cyan"
+                shadow={"md"}
+                onClick={() =>
+                  window.open(
+                    "https://drive.google.com/file/d/1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as/view?usp=sharing",
+                    "_blank",
+                    "noreferrer"
+                  )
+                }
+              >
+                RESUME →
+              </Button>
+            </Flex>
           </Box>
-          <Flex ml="25%" mt="20px">
-            <Button
-              w="fit-content"
-              size="sm"
-              borderRightRadius={"none"}
-              colorScheme="cyan"
-              shadow={"md"}
-            >
-              MORE ABOUT ME →
-            </Button>
-            <Button
-              w="fit-content"
-              size="sm"
-              borderLeftRadius={"none"}
-              variant={"outline"}
-              colorScheme="cyan"
-              shadow={"md"}
-              onClick={() =>
-                window.open(
-                  "https://drive.google.com/file/d/1wmufWjrLaIsUqdE_0nBDweoFlVvtq_as/view?usp=sharing",
-                  "_blank",
-                  "noreferrer"
-                )
-              }
-            >
-              RESUME →
-            </Button>
-          </Flex>
         </Box>
 
         <Box
           bgColor="#0BC5EA"
-          w="50%"
+          w={{ base: "100%", md: "50%", lg: "50%" }}
+          h={{ base: "60%", md: "100%", lg: "100%" }}
           overflow={"hidden"}
           boxShadow={"lg"}
           position={"relative"}
         >
-          <Image src="avhomeimage.png" w="590px" m="auto" />
+          <Image
+            src="avhomeimage.png"
+            w="87%"
+            // w={{ base: "100%", md: "87%", lg: "87%" }}
+            // border="1px solid black"
+            position="absolute"
+            bottom="0"
+            left="50%"
+            transform="translateX(-50%)"
+            m="auto"
+          />
         </Box>
       </Flex>
     </Box>
